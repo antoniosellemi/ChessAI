076dd14e1d4e32f566b6ad262c4c946b5eb834d7
antoniosellemi <antonio.a.sellemi@gmail.com>
2022-07-02 01:07:29 -0400

    added functionality for piece movement

--- Chess/chessmain.py
+++ Chess/chessmain.py
@@ -14,7 +14,7 @@ Load in Images into Dictionary
 """
 
 
-def getimages():
+def get_images():
     pieces = ["wp", "bp", "wR", "bR", "wN", "bN", "wB", "bB", "wQ", "bQ", "wK", "bK"]
     for piece in pieces:
         IMAGES[piece] = game.transform.scale(game.image.load("Images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
@@ -25,18 +25,36 @@ Handles User Input and Updating Graphics
 """
 
 
-def rungame():
-    getimages()
+def run_game():
+    get_images()
     game.init()
     screen = game.display.set_mode((WIDTH, HEIGHT))
     clock = game.time.Clock()
     screen.fill(game.Color("white"))
     bs = engine.BoardState()
     playing = True
+    player_mouse_clicks = []
+    selected = ()
     while playing:
         for event in game.event.get():
             if event.type == game.QUIT:
                 playing = False
+            elif event.type == game.MOUSEBUTTONDOWN:
+                square = game.mouse.get_pos()
+                file = square[0]//SQUARE_SIZE
+                rank = square[1]//SQUARE_SIZE
+                if selected == (rank, file):
+                    selected = ()
+                    player_mouse_clicks = []
+                else:
+                    selected = (rank, file)
+                    player_mouse_clicks.append(selected)
+                if len(player_mouse_clicks) == 2:
+                    move = engine.PieceMove(player_mouse_clicks[0], player_mouse_clicks[1], bs.board)
+                    bs.make_move(move)
+                    selected = ()
+                    player_mouse_clicks = []
+        draw_game(bs, screen)
         clock.tick(MAX_FPS)
         game.display.flip()
 
@@ -46,9 +64,9 @@ Handles graphics of given board state
 """
 
 
-def drawgame(bs, screen):
-    drawboard(screen)
-    drawpieces(screen, bs.board)
+def draw_game(bs, screen):
+    draw_board(screen)
+    draw_pieces(screen, bs.board)
 
 
 """
@@ -56,21 +74,20 @@ Helpers
 """
 
 
-def drawboard(screen):
-    colors = [game.Color("white"), game.Color("violet")]
+def draw_board(screen):
+    colors = [game.Color("light yellow"), game.Color("dark green")]
     for r in range(DIMENSION):
         for f in range(DIMENSION):
-            color = colors[(r + f) % 2]  # Dark squares will have remainder 1
-            game.draw.rect(screen, color, game.Rect(f * SQUARE_SIZE, f * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            color = colors[(r + f) % 2]  # Dark squares will have a remainder 1
+            game.draw.rect(screen, color, game.Rect(f*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 
-def drawpieces(screen, board):
+def draw_pieces(screen, board):
     for r in range(DIMENSION):
         for f in range(DIMENSION):
             piece = board[r][f]
             if piece != "..":
-                screen.blit(IMAGES[piece], game.Rect(f * SQUARE_SIZE, f * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                screen.blit(IMAGES[piece], game.Rect(f * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 
-if __name__ == '__main__':
-    rungame()
+run_game()
--- Chess/engine.py
+++ Chess/engine.py
@@ -14,4 +14,30 @@ class BoardState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
         self.log = []
-        self.whiteToMove = True
+        self.white_to_move = True
+
+    def make_move(self, piecemove):
+        self.board[piecemove.start_rank][piecemove.start_file] = ".."
+        self.board[piecemove.end_rank][piecemove.end_file] = piecemove.piece_moved
+        self.log.append(piecemove)
+        self.white_to_move = not self.white_to_move
+
+class PieceMove():
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+    files_to_cols = {"h": 7, "g": 6, "f": 5, "e": 4, "d": 3, "c": 2, "b": 1, "a": 0}
+    cols_to_files = {v: k for k, v in files_to_cols.items()}
+
+    def __init__(self, start_sq, end_sq, board):
+        self.start_rank = start_sq[0]
+        self.start_file = start_sq[1]
+        self.end_rank = end_sq[0]
+        self.end_file = end_sq[1]
+        self.piece_moved = board[self.start_rank][self.start_file]
+        self.piece_captured = board[self.end_rank][self.end_file]
+
+    def get_chess_notation_square(self):
+        return self.get_rank_and_file(self.start_rank, self.start_file) + self.get_rank_and_file(self.end_file, self.end_file)
+
+    def get_rank_and_file(self, r, c):
+        return self.cols_to_files[c] + self.rows_to_ranks[r]
